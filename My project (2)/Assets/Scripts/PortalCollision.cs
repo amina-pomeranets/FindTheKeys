1dd10302e3e96bf5a2f809aaea139255cd337c70
amina-pomeranets <166169032+amina-pomeranets@users.noreply.github.com>
2024-09-19 17:20:05 -0400

    Started To Design Level 2
    Despite still trying to get the portal to work, I started to design level 2. I believe I might know what the objective of the level will be, but I am still brainstorming.

--- My project (2)/Assets/Scripts/PortalCollision.cs
+++ My project (2)/Assets/Scripts/PortalCollision.cs
@@ -7,25 +7,35 @@ using UnityEngine.SceneManagement;
 
 public class PortalCollision : MonoBehaviour
 {
+    public string sceneName;  // The name of the scene to switch to
 
-    //public int keyCount;
-
-    public GameObject Portal;
-
-    public Boolean holdingKey = false;
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player"))  // Check if the player enters the portal
+        {
+            SceneManager.LoadScene(sceneName);  // Load the specified scene
+        }
+    }
+}
+
+//public int keyCount;
+
+//public GameObject Portal;
+
+//public Boolean holdingKey = false;
+
+// Start is called before the first frame update
+//void Start()
+    //{
         //keyCount = GameObject.FindGameObjectsWithTag("Key").Length;
 
         //Portal.SetActive(false);
         //holdingKey = false;
-    }
+    //}
 
      //Update is called once per frame
-    void Update ()
-    {
+    //void Update ()
+    //{
         //keyCount = GameObject.FindGameObjectsWithTag("Key").Length;
         //Portal.SetActive(true);
 
@@ -33,21 +43,24 @@ public class PortalCollision : MonoBehaviour
         //{
             //Portal.SetActive(true);
         //}
-    }
+    //}
 
 
 
-
+/*
     void OnCollisionEnter2D(Collision2D collision)
     {
         print("Player touched");
         if (collision.gameObject.tag == "Key")
         {
             //Portal.SetActive(true);
-            holdingKey = true;
+            //holdingKey = true;
 
             print("SceneChange");
             SceneManager.LoadScene(1);
         }
     }
-}
+
+
+*/
+//}
